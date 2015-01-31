e14daf6b39a681d39afe8ab9b1f7e0559596b081
Keith Lynch <spleef79@gmail.com>
2015-01-31 09:47:07 -0600

    Update init_trlte.c

--- init/init_trlte.c
+++ init/init_trlte.c
@@ -56,7 +56,7 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
     if (strstr(bootloader, "N910R4")) {
         /* trlteusc */
         property_set("ro.build.fingerprint", "samsung/trlteusc/trlteusc:4.4.4/KTU84P/N910R4USC1ANK5:user/release-keys");
-        property_set("ro.build.description", "trlteusc-user 4.4.4 KTU84P N910R4USC1ANK5 release-keys");
+        property_set("ro.build.description", "trlteusc-user 5.0.2 LRX22G N910R4USC1ANK5 release-keys");
         property_set("ro.product.model", "SM-N910R4");
         property_set("ro.product.device", "trlteusc");
         cdma_properties("0", "311580", "U.S.Cellular");
